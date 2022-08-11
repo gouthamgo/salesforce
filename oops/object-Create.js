ffafd60244cf10d3bc90e047c2c1e78622bb3359
goutham m <gouthamgo@yahoo.com>
2022-08-11 10:35:55 +1000

    object-create

---
+++ oops/object-Create.js
@@ -0,0 +1,24 @@
+// Onject create method 
+
+//  create a simple object with properties 
+
+const coder = {
+    isStudying: false,
+    printIntro: function(){
+        console.log(`my name is ${this.name}, Am I studying? ${this.isStudying}`)
+    }
+}
+
+// Object create method
+
+const me = Object.create(coder);
+
+//  name is the property set on 'me' , but not on the coder
+
+me.name = 'Rakul';
+
+// Inherited properties can be overwritten
+
+me.isStudying = true;
+
+me.printIntro();
